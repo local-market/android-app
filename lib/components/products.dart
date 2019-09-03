1a39622261f478d6b32a556d861c369f275f5c6d
Chhekur <820121223505e@gmail.com>
2019-09-03 20:10:09 +0530

    Merged: Chhekur & deveshpankaj's version

---
+++ lib/components/products.dart
@@ -0,0 +1,126 @@
+import 'package:flutter/material.dart';
+
+
+import 'package:local_market/components/product_details.dart';
+
+
+class Products extends StatefulWidget {
+  @override
+  _ProductsState createState() => _ProductsState();
+}
+
+class _ProductsState extends State<Products> {
+  var product_list = [
+    {
+      "name": "Red dress",
+      "picture": "assets/products/dress1.jpeg",
+      "old_price": 100,
+      "price": 50,
+      "currency":"rupee",
+    },
+    {
+      "name": "Black",
+      "picture": "assets/products/dress2.jpeg",
+      "old_price": 120,
+      "price": 85,
+      "currency":"rupee",
+    },
+    {
+      "name": "Blazer 1",
+      "picture": "assets/products/blazer1.jpeg",
+      "old_price": 120,
+      "price": 85,
+      "currency":"rupee",
+    },
+    {
+      "name": "Blazer 2",
+      "picture": "assets/products/blazer2.jpeg",
+      "old_price": 120,
+      "price": 85,
+      "currency":"rupee",
+    },
+  ];
+  @override
+  Widget build(BuildContext context) {
+    return GridView.builder(
+      itemCount: product_list.length,
+      gridDelegate:
+          SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 2),
+      itemBuilder: (BuildContext context, int index){
+        return Single_prod(
+          prod_name: product_list[index]['name'],
+          prod_pricture: product_list[index]['picture'],
+          prod_old_price: product_list[index]['old_price'],
+          prod_price: product_list[index]['price'],
+          currency: product_list[index]['currency'],
+        );
+      },
+    );
+  }
+}
+
+class Single_prod extends StatelessWidget {
+  final currency_symbol = {
+    "rupee": '₹',
+  };
+  final prod_name;
+  final prod_pricture;
+  final prod_old_price;
+  final prod_price;
+  final currency;
+
+  Single_prod({
+    this.prod_name,
+    this.prod_pricture,
+    this.prod_old_price,
+    this.prod_price,
+    this.currency,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      child: Hero(
+          tag: prod_name,
+          child: Material(
+            child: InkWell(
+              onTap: () => Navigator.of(context).push(
+            new MaterialPageRoute(builder: (context) => new ProductDetails(
+              prod_name: prod_name,
+              prod_pricture: prod_pricture,
+              prod_old_price: prod_old_price,
+              prod_price: prod_price,
+              currency: currency,
+            ))),
+              child: GridTile(
+                  footer: Container(
+                    color: Colors.white70,
+                    child: ListTile(
+                        leading: Text(
+                          prod_name,
+                          style: TextStyle(fontWeight: FontWeight.bold),
+                        ),
+                        title: Text(
+                          "${currency_symbol[currency]} $prod_price",
+                          style: TextStyle(
+                              color: Colors.red, fontWeight: FontWeight.w800),
+                        ),
+                        subtitle: Text(
+                          "${currency_symbol[currency]} $prod_old_price",
+                          style: TextStyle(
+                              color: Colors.black54,
+                              fontWeight: FontWeight.w800,
+                              decoration
+                                  :TextDecoration.lineThrough),
+                        ),
+                    ),
+                  ),
+                  child: Image.asset(
+                    prod_pricture,
+                    fit: BoxFit.cover,
+                  )),
+            ),
+          )),
+    );
+  }
+}
--- lib/views/home.dart
+++ lib/views/home.dart
@@ -1,10 +1,12 @@
 import "package:flutter/material.dart";
+import 'package:local_market/components/horizontal_slide.dart';
+import 'package:local_market/components/products.dart';
 import "package:local_market/controller/user_controller.dart";
 import "package:firebase_auth/firebase_auth.dart";
 import 'package:local_market/utils/utils.dart';
 import "package:local_market/views/login.dart";
-
 import 'add_product.dart';
+import "package:carousel_pro/carousel_pro.dart";
 
 class Home extends StatefulWidget {
   @override
@@ -18,30 +20,149 @@ class _HomeState extends State<Home> {
 
   @override
   Widget build(BuildContext context) {
+    Widget images_carousel = new Container(
+      height: 200.0,
+      child: new Carousel(
+        boxFit: BoxFit.cover,
+        images: [
+          AssetImage('assets/img/c1.jpg'),
+          AssetImage('assets/img/m1.jpeg'),
+          AssetImage('assets/img/m2.jpg'),
+          AssetImage('assets/img/w1.jpeg'),
+          AssetImage('assets/img/w3.jpeg'),
+          AssetImage('assets/img/w4.jpeg'),
+        ],
+        autoplay: true,
+        animationCurve: Curves.fastOutSlowIn,
+        animationDuration: Duration(milliseconds: 1000),
+        dotSize: 4.0,
+        dotColor: Colors.green,
+        indicatorBgPadding: 4.0,
+      ),
+    );
     return Scaffold(
-      appBar: AppBar(),
-      body: Center(
-        child: ListView(
+      appBar: AppBar(
+        backgroundColor: Colors.red,
+        title: Text('Apni dukan'),
+        actions: <Widget>[
+          new IconButton(
+              icon: Icon(
+                Icons.search,
+                color: Colors.white,
+              ),
+              onPressed: null),
+          new IconButton(
+              icon: Icon(
+                Icons.shopping_cart,
+                color: Colors.white,
+              ),
+              onPressed: null)
+        ],
+      ),
+      drawer: new Drawer(
+        child: new ListView(
           children: <Widget>[
-            Material(
-              color: Colors.red,
-              child: MaterialButton(onPressed: (){
-                userController.logout();
-              }, child: Text("Logout", style: TextStyle(
-                color:Colors.white,
-              ),)),
+            new UserAccountsDrawerHeader(
+              accountName: Text("Pankaj Devesh"),
+              accountEmail: Text('pankajdevesh3@gmail.com'),
+              currentAccountPicture: GestureDetector(
+                child: new CircleAvatar(
+                  backgroundColor: Colors.white,
+                  backgroundImage: NetworkImage(
+                      'https://avatars1.githubusercontent.com/u/28962789'),
+                ),
+              ),
+              decoration: BoxDecoration(
+                color: Colors.red,
+              ),
             ),
-            Material(
-              color: Colors.red,
-              child: MaterialButton(onPressed: (){
-                Navigator.push(context, MaterialPageRoute(builder: (context) => AddProduct()));
-              }, child: Text("Add Product", style: TextStyle(
-                color:Colors.white,
-              ),)),
+            InkWell(
+              onTap: () {},
+              child: ListTile(
+                title: Text("Home"),
+                leading: Icon(Icons.person, color: Colors.pink),
+              ),
+            ),
+            InkWell(
+              onTap: () {},
+              child: ListTile(
+                title: Text("My Account"),
+                leading: Icon(Icons.shopping_basket, color: Colors.pink),
+              ),
+            ),
+            InkWell(
+              onTap: () {},
+              child: ListTile(
+                title: Text("My Order"),
+                leading: Icon(Icons.dashboard, color: Colors.pink),
+              ),
+            ),
+            InkWell(
+              onTap: () {},
+              child: ListTile(
+                title: Text("Favourites"),
+                leading: Icon(Icons.favorite, color: Colors.pink),
+              ),
+            ),
+            Divider(),
+            InkWell(
+              onTap: () {},
+              child: ListTile(
+                title: Text("Settings"),
+                leading: Icon(Icons.settings),
+              ),
+            ),
+            InkWell(
+              onTap: () {},
+              child: ListTile(
+                title: Text("Help"),
+                leading: Icon(
+                  Icons.help,
+                  color: Colors.blue,
+                ),
+              ),
+            ),
+            InkWell(
+              onTap: () {
+                userController.logout();
+                Navigator.pushReplacement(context, MaterialPageRoute(builder: (context) => Home()));
+              },
+              child: ListTile(
+                title: Text("Logout", style: TextStyle(color: Colors.black26),),
+                leading: Icon(
+                  Icons.warning,
+                  color: Colors.yellow,
+                ),
+              ),
             ),
           ],
-        )
-      )
+        ),
+      ),
+      body: new ListView(
+        children: <Widget>[
+          images_carousel,
+
+          // padding
+          new Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Text("Products"),
+          ),
+
+          // horizintal list view
+          HorizontalList(),
+
+          // padding
+          new Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Text("New"),
+          ),
+
+          new Container(
+            height: 300.0,
+            child: Products(),
+          )
+        ],
+      ),
     );
   }
 
