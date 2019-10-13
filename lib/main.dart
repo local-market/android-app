bdd101c39a3524e5894973e85ad0c5e61c263b35
Pankaj Devesh <pankajdevesh3@gmail.com>
2019-10-13 20:04:22 +0530

    Config:

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,24 @@
+import 'package:camera/camera.dart';
 import 'package:flutter/material.dart';
 import 'package:local_market/utils/utils.dart';
 import "package:local_market/views/home.dart";
+import 'package:local_market/components/counter.dart';
 import 'package:flutter/services.dart';
 
-void main(){
+Future<void> main() async {
   SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle(
     statusBarColor: Colors.white,
     statusBarBrightness: Brightness.light,
   ));
+
+
+// Obtain a list of the available cameras on the device.
+  final cameras = await availableCameras();
+
+  // Get a specific camera from the list of available cameras.
+  final firstCamera = cameras.first;
+
+
   runApp(
     new MaterialApp(
       debugShowCheckedModeBanner: false,
@@ -17,7 +28,9 @@ void main(){
       ),
       home: AnnotatedRegion<SystemUiOverlayStyle>(
         value: SystemUiOverlayStyle.dark,
-        child: Home()
+
+        /// replacing Home with counter
+        child: TakePictureScreen(camera: firstCamera,)
       ),
     )
   );
