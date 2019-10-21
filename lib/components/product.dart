540a30f386719b6e82d8cb4a6c1b0660f4e3e67a
YSJ <thakurysj@gmail.com>
2019-10-21 17:26:05 +0530

    Fixed: products page width reduced issue

--- lib/components/product.dart
+++ lib/components/product.dart
@@ -35,114 +35,119 @@ class _ProductState extends State<Product> {
 
   @override
   Widget build(BuildContext context) {
-    return Stack(
-      children: <Widget>[
-        Card(
+//    return Stack(
+//      children: <Widget>[
+        return Card(
           child: Container(
               width: (MediaQuery
                   .of(context)
                   .size
                   .width / 2) - 40,
               // height: 110.0,
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
+              child: Stack(
                 children: <Widget>[
-                  Container(
-                    height: 130,
-                    child: InkWell(
-                      onTap: () {
-                        Navigator.push(
-                            context, CupertinoPageRoute(builder: (context) =>
-                            ProductView(
-                                this._product
-                            )));
-                      },
-                      child: Padding(
-                        padding: const EdgeInsets.all(20.0),
-                        child: Center(
-                          child: Image.network(
-                            this._product['image'],
-                            fit: BoxFit.cover,
-                            // width: double.infinity,
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: <Widget>[
+                      Container(
+                        height: 130,
+                        child: InkWell(
+                          onTap: () {
+                            Navigator.push(
+                                context, CupertinoPageRoute(builder: (context) =>
+                                ProductView(
+                                    this._product
+                                )));
+                          },
+                          child: Padding(
+                            padding: const EdgeInsets.all(20.0),
+                            child: Center(
+                              child: Image.network(
+                                this._product['image'],
+                                fit: BoxFit.cover,
+                                // width: double.infinity,
+                              ),
+                            ),
                           ),
                         ),
                       ),
-                    ),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.fromLTRB(8.0, 0, 8.0, 0),
-                    child: InkWell(
-                      onTap: () {
-                        Navigator.push(
-                            context, CupertinoPageRoute(builder: (context) =>
-                            ProductView(
-                                this._product
-                            )));
-                      },
-                      child: Text(
-                        this._product['name'].length > 20 ? this
-                            ._product['name'].substring(0, 20) + "..." : this
-                            ._product['name'],
-                        style: TextStyle(
-                            fontSize: 15
-                        ),
+                      Padding(
+                        padding: const EdgeInsets.fromLTRB(8.0, 0, 8.0, 0),
+                        child: InkWell(
+                          onTap: () {
+                            Navigator.push(
+                                context, CupertinoPageRoute(builder: (context) =>
+                                ProductView(
+                                    this._product
+                                )));
+                          },
+                          child: Text(
+                            this._product['name'].length > 20 ? this
+                                ._product['name'].substring(0, 20) + "..." : this
+                                ._product['name'],
+                            style: TextStyle(
+                                fontSize: 15
+                            ),
 
+                          ),
+                        ),
                       ),
-                    ),
+                      Padding(
+                          padding: const EdgeInsets.fromLTRB(8.0, 8, 8.0, 0),
+                          child: RichText(
+                            text: TextSpan(
+                                children: [
+                                  TextSpan(
+                                    text: '₹${this._product['offerPrice']}  ',
+                                    style: TextStyle(
+                                      fontSize: 15,
+                                      color: _utils.colors['theme'],
+
+                                    ),
+                                  ),
+                                  TextSpan(
+                                    text: '₹${this._product['price']}',
+                                    style: TextStyle(
+                                        fontSize: 13,
+                                        decoration: TextDecoration.lineThrough,
+                                        color: Colors.grey.shade700
+                                    ),
+                                  )
+                                ]
+                            ),
+                          )
+                        // Text('₹ ${this._product['price']}',
+                        //   style: TextStyle(
+                        //     fontSize: 15
+                        //   ),
+                        // ),
+                      ),
+                      AddButton(this._product, null, null, this._inner)
+                    ],
                   ),
                   Padding(
-                      padding: const EdgeInsets.fromLTRB(8.0, 8, 8.0, 0),
-                      child: RichText(
-                        text: TextSpan(
-                            children: [
-                              TextSpan(
-                                text: '₹${this._product['offerPrice']}  ',
-                                style: TextStyle(
-                                  fontSize: 15,
-                                  color: _utils.colors['theme'],
+                    padding: const EdgeInsets.all(12.0),
+                    child: CircleAvatar(
+                      radius: 18,
+                      child: Text(
 
-                                ),
-                              ),
-                              TextSpan(
-                                text: '₹${this._product['price']}',
-                                style: TextStyle(
-                                    fontSize: 13,
-                                    decoration: TextDecoration.lineThrough,
-                                    color: Colors.grey.shade700
-                                ),
-                              )
-                            ]
+                        "${(((double.parse(this._product['price']) - double.parse(this._product['offerPrice'])) / double.parse(this._product['price']) ) * 100) .ceil() }%\n off",
+                        style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 12,
+                            fontWeight: FontWeight.bold
                         ),
-                      )
-                    // Text('₹ ${this._product['price']}',
-                    //   style: TextStyle(
-                    //     fontSize: 15
-                    //   ),
-                    // ),
-                  ),
-                  AddButton(this._product, null, null, this._inner)
+                      ),backgroundColor: Colors.red.shade700,
+                    ),
+                  )
                 ],
               )
           ),
-        ),
+        );
 
-        Padding(
-          padding: const EdgeInsets.all(12.0),
-          child: CircleAvatar(
-            radius: 18,
-            child: Text(
 
-              "${(((double.parse(this._product['price']) - double.parse(this._product['offerPrice'])) / double.parse(this._product['price']) ) * 100) .ceil() }%\n off",
-              style: TextStyle(
-                color: Colors.white,
-                fontSize: 12,
-                fontWeight: FontWeight.bold
-              ),
-            ),backgroundColor: Colors.red.shade700,
-          ),
-        )
 
-      ],
-    );
+//      ],
+//    );
   }
 }
